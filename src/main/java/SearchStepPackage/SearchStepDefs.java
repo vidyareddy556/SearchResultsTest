9710413d4448277b7f6719625fa9dd1d9c7a05ef
vidyareddy <vidyareddy556@gmail.com>
2023-01-21 20:55:48 -0600

    Update Readme

--- src/main/java/SearchStepPackage/SearchStepDefs.java
+++ src/main/java/SearchStepPackage/SearchStepDefs.java
@@ -3,15 +3,13 @@ package SearchStepPackage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.time.Duration;
-
 import static org.junit.Assert.assertTrue;
 
 public class SearchStepDefs {
@@ -28,7 +26,7 @@ public class SearchStepDefs {
     public void searchForSomething(String query) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         //WebElement element = driver.findElement(By.name("q"));
-       // WebElement element = driver.findElement(By.xpath("//input[@type='search']"));
+        // WebElement element = driver.findElement(By.xpath("//input[@type='search']"));
         WebElement element = driver.findElement(By.xpath("//*[@title='Search']"));
         // Enter something to search for
         element.sendKeys(query);
@@ -37,17 +35,10 @@ public class SearchStepDefs {
 
     @Then("I should see results for {string}")
     public void verifyResults(String searchResults) {
-        WebElement resultsContainer = driver.findElement(By.id("main"));
+        WebElement resultsContainer = driver.findElement(By.id("rso"));
         int results = resultsContainer.findElements(By.tagName("li")).size();
         assertTrue(results > 0);
         driver.quit();
-    }
-
-    @Then("I should see a message {string}")
-    public void verifyNoResultsMessage(String searchResults){
-        WebElement resultsContainer = driver.findElement(By.id("main"));
-        int results = resultsContainer.findElements(By.tagName("li")).size();
-        assertTrue(results > 0);
     }
 
 }
--- src/test/java/Cucumbersearch_package/search.feature
+++ src/test/java/Cucumbersearch_package/search.feature
@@ -6,8 +6,3 @@ Feature: Search Functionality
     Given I am on the homepage
     When I search for "Swagger UI"
     Then I should see results for "Swagger UI"
-
-  Scenario: Search for tfdgthjfhfdfxhtdhjyf
-    Given I am on the homepage
-    When I search for "tfdgthjfhfdfxhtdhjyf"
-    Then I should see a message "did not match any documents"
